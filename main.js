6b14d6fd2671aaff3c63918348e19c3ded959adf
kmr2007 <kaydenceriehl@gmail.com>
2023-02-16 13:27:29 -0700

    bfxbh

--- main.js
+++ main.js
@@ -1,2 +1,30 @@
 // Program Title
-console.log("hi");
+//Variable
+let boardA1 = document.getElementById("a1");
+let boardA2 = document.getElementById("a2");
+let boardA3 = document.getElementById("a3");
+let boardB1 = document.getElementById("b1");
+let boardB2 = document.getElementById("b2");
+let boardB3 = document.getElementById("b3");
+let boardC1 = document.getElementById("c1");
+let boardC2 = document.getElementById("c2");
+let boardC3 = document.getElementById("c3");
+
+//Event Listeners
+boardA1.addEventListener("click", boardA1click);
+boardA2.addEventListener("click", boardA2click);
+boardA3.addEventListener("click", boardA3click);
+boardB1.addEventListener("click", boardB1click);
+boardB2.addEventListener("click", boardB2click);
+boardB3.addEventListener("click", boardB3click);
+boardC1.addEventListener("click", boardC1click);
+boardC2.addEventListener("click", boardC2click);
+boardC3.addEventListener("click", boardC3click);
+
+//Player 1 + 2
+
+// Space Clicks
+
+function boardA1click() {
+  console.log("hi");
+}
